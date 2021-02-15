e84a7cfede96fcefd7e0e887fc1542c90afc38ae
RealStack-Apps <admin@realstackapps.com>
2021-02-15 08:26:51 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
-const log = function(entry) {
-    fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
-};
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const fs = require('fs');
-const port = process.env.PORT || 3000,
-    http = require('http'),
-    html = fs.readFileSync('index.html');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -22,4 +17,4 @@ const server = app.listen(3000, () => {
 
 
 // a friendly message on the terminal
-console.log('Server running at http://127.0.0.1:' + port + '/');
+// console.log('Server running at http://127.0.0.1:' + port + '/');
